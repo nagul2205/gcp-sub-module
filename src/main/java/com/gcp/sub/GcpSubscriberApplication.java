c0889e2903861369e2fce527ba5832f74eb865fe
nagul2205 <shaiknagulbasha2205@gmail.com>
2025-11-01 19:41:31 +0530

    Initial Commit

---
+++ src/main/java/com/gcp/sub/GcpSubscriberApplication.java
@@ -0,0 +1,13 @@
+package com.gcp.sub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GcpSubscriberApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GcpSubscriberApplication.class, args);
+	}
+
+}
